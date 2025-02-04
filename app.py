216c2a807c21bee40f895565089fa3e7d7dd8083
aswinal22 <aswin.al22@bitsathy.ac.in>
2025-02-04 15:42:35 +0530

    Fixed port binding for Render

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def predict():
     
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, port=5005)  # Flask app runs on port 5000
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a port dynamically
+    app.run(host='0.0.0.0', port=port, debug=True)
